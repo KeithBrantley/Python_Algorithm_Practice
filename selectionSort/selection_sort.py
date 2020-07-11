112e6fc1e8c7dbb3fb43d020bb723645d2c00cbc
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-11 12:22:43 -0700

    added comments

--- selectionSort/selection_sort.py
+++ selectionSort/selection_sort.py
@@ -2,17 +2,17 @@
 
 def selection_sort(list_a):
     indexing_length = range(0, len(list_a)-1)
-
+# declare a default for the min value
     for i in indexing_length:
         min_value = i
-
+# if value at j index is < current min value, replace the min value
         for j in range(i+1, len(list_a)):
             if list_a[j] < list_a[min_value]:
                 min_value = j
-
+# if the min value is lower then default switch the value
         if min_value != i:
             list_a[min_value], list_a[i] = list_a[i], list_a[min_value]
-
+# return the list
     return list_a
 
 print(selection_sort([9,7,8,4,6,5,1,3,2]))
